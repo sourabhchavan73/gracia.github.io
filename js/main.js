c37791e2ae33886a5a2fc92158f2ab16475e4b40
sourabhchavan73 <sourabh@brandzgarage.com>
2020-08-24 12:35:27 +0530

    changes

--- js/main.js
+++ js/main.js
@@ -14,15 +14,7 @@
     });
 
     // map
-    function initMap() {
-        // The location of Uluru
-        var uluru = {lat: -25.344, lng: 131.036};
-        // The map, centered at Uluru
-        var map = new google.maps.Map(
-      document.getElementById('map'), {zoom: 4, center: uluru});
-        // The marker, positioned at Uluru
-        var marker = new google.maps.Marker({position: uluru, map: map});
-  }
+
   
   
   //scroll on mobile
@@ -67,3 +59,9 @@
       $('#mynav').addClass('navbar-padding');
     }
 });
+
+// lightbox
+$(document).on('click', '[data-toggle="lightbox"]', function(event){
+  event.preventDefault();
+  $(this).ekkoLightbox();
+})
